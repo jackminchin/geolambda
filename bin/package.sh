438a3bba14426b83bcae52e7825b6ae40a6451ec
Jack Minchin <jackm@custom-group.com>
2021-08-18 21:29:06 +0100

    latest test

--- bin/package.sh
+++ bin/package.sh
@@ -4,18 +4,20 @@
 export DEPLOY_DIR=lambda
 
 # make deployment directory and add lambda handler
-mkdir -p $DEPLOY_DIR/lib
+mkdir -p $DEPLOY_DIR
 
 # copy libs
-cp -P ${PREFIX}/lib/*.so* $DEPLOY_DIR/lib/
-cp -P ${PREFIX}/lib64/libjpeg*.so* $DEPLOY_DIR/lib/
+# cp -P ${PREFIX}/lib/*.so* $DEPLOY_DIR/lib/
+# cp -P ${PREFIX}/lib64/libjpeg*.so* $DEPLOY_DIR/lib/
+cp -P -r /var/task/* $DEPLOY_DIR
 
-strip $DEPLOY_DIR/lib/* || true
+
+strip $DEPLOY_DIR/* || true
 
 # copy GDAL_DATA files over
-mkdir -p $DEPLOY_DIR/share
-rsync -ax $PREFIX/share/gdal $DEPLOY_DIR/share/
-rsync -ax $PREFIX/share/proj $DEPLOY_DIR/share/
+# mkdir -p $DEPLOY_DIR/share
+# rsync -ax $PREFIX/share/gdal $DEPLOY_DIR/share/
+# rsync -ax $PREFIX/share/proj $DEPLOY_DIR/share/
 
 # zip up deploy package
 cd $DEPLOY_DIR
